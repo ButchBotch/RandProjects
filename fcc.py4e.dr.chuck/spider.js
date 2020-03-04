2a1fc670ea2de88d1e920c7819341ec833ca1b43
Kyzalas Stavros <stkyzal@gmail.com>
2020-03-04 14:12:56 +0200

    putitin

---
+++ fcc.py4e.dr.chuck/spider.js
@@ -0,0 +1,10 @@
+spiderJson = {"nodes":[
+{"weight":5,"rank":15.85767605246199, "id":1, "url":"http://www.dr-chuck.com"},
+{"weight":4,"rank":19.0, "id":2, "url":"http://www.dr-chuck.com/dr-chuck/resume/index.htm"},
+{"weight":1,"rank":2.084607524403774, "id":3, "url":"http://www.dr-chuck.com/office"},
+{"weight":1,"rank":0.0, "id":4, "url":"http://www.dr-chuck.com/sakai-book"}],
+"links":[
+{"source":0,"target":1,"value":3},
+{"source":0,"target":2,"value":3},
+{"source":0,"target":3,"value":3},
+{"source":1,"target":0,"value":3}]};
